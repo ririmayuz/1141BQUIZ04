298e6cfe341c424f63714cf2ebd8692fe0ef0585
Yuz <wwowkdrr@gmail.com>
2025-08-12 11:32:17 +0800

    會員註冊 登入頁面完成

--- front/login.php
+++ front/login.php
@@ -1,1 +1,22 @@
-商品
+<h2>第一次登入</h2>
+<a href="?do=reg">
+    <img src="./icon/0413.jpg" alt="">
+</a>
+<h2>會員登入</h2>
+
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="chk" id="chk"></td>
+    </tr>
+</table>
+
+<div class="ct"><button>確認</button></div>
