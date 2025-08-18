7b6569377d622d86acdbceb8f4443428fa14ba45
Yuz <wwowkdrr@gmail.com>
2025-08-18 09:48:58 +0800

    完成新增中分類功能

---
+++ api/save_type.php
@@ -0,0 +1,8 @@
+<?php include_once "db.php";
+
+// $_POST['pr']=serialize($_POST['pr']);
+
+$Type->save($_POST);
+
+
+
--- back/th.php
+++ back/th.php
@@ -3,14 +3,14 @@
 <div class="ct">
     新增大分類
     <input type="text" name="big" id="big">
-    <button>新增</button>
+    <button onclick="addBig()">新增</button>
 </div>
 <!-- div.ct>select+input:text+button -->
  <div class="ct">
     新增中分類
     <select name="selBig" id="selBig"></select>
     <input type="text" name="mid" id="mid">
-    <button>新增</button>
+    <button onclick="addMid()">新增</button>
 </div>
 
 <!-- table.all>(tr.tt>td+td.ct>button*2) -->
@@ -31,6 +31,30 @@
     </tr>
  </table>
 
+ <script>
+getBigs();    
+function addBig(){
+    let name=$("#big").val();
+    $.post("./api/save_type.php",{name,big_id:0},()=>{
+        $("#big").val("");
+        getBigs();
+    })
+}    
+
+function addMid(){
+    let name=$("#mid").val();
+    let big_id=$("#selBig").val();
+    $.post("./api/save_type.php",{name,big_id},()=>{
+        location.reload()
+    })
+} 
+
+function getBigs(){
+    $.get("./api/get_bigs.php",(options)=>{
+        $("#selBig").html(options);
+    })
+}
+ </script>
 
 <h2 class="ct">商品管理</h2>
 <div class="ct">
@@ -42,7 +66,7 @@
         <td>商品名稱</td>
         <td>庫存量</td>
         <td>狀態</td>
-        <td>操作</td>
+        <td>操</td>
     </tr>
     <tr class="pp ct">
         <td></td>
