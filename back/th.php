9cc14e0763f4157362f835e78a33ea5e6a2bc2a4
Yuz <wwowkdrr@gmail.com>
2025-08-18 14:40:36 +0800

    additem

--- back/th.php
+++ back/th.php
@@ -96,15 +96,16 @@ $(".edit-btn").on("click",function(){
 
 <h2 class="ct">商品管理</h2>
 <div class="ct">
-    <button>新增商品</button>
+    <button onclick="location.href='?do=add_item'">新增商品</button>
 </div>
  <table class="all">
+
     <tr class="tt ct">
         <td>編號</td>
         <td>商品名稱</td>
         <td>庫存量</td>
         <td>狀態</td>
-        <td>操</td>
+        <td>操作</td>
     </tr>
     <tr class="pp ct">
         <td></td>
