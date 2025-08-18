5e5c970baf80b21673bfff25a1bee1acc241663c
Yuz <wwowkdrr@gmail.com>
2025-08-18 08:13:31 +0800

    修正程式2

--- api/db.php
+++ api/db.php
@@ -1,17 +1,20 @@
 <?php
+date_default_timezone_set('Asia/Taipei');
 session_start();
-date_default_timezone_set("Asia/Taipei");
+
 function dd($array){
-    echo "<pre>";
+    echo '<pre>';
     print_r($array);
-    echo "</pre>";
+    echo '</pre>';
 }
 
+
 function q($sql){
-    $dsn='mysql:host=localhost;dbname=db11;charset=utf8';
+    $dsn="mysql:host=localhost;charset=utf8;dbname=db11";
     $pdo=new PDO($dsn,'root','');
     return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
+
 
 function to($url){
     header("location:".$url);
@@ -48,43 +51,6 @@ function all(...$arg){
 
 function count(...$arg){
     $sql="select count(*) from $this->table ";
-    if(isset($arg[0])){
-        if(is_array($arg[0])){
-            $tmp=$this->arraytosql($arg[0]);
-            $sql=$sql." where ".join(" AND " , $tmp);
-
-        }else{
-            $sql .= $arg[0];
-        }
-    }
-
-    if(isset($arg[1])){
-        $sql .= $arg[1];
-    }
-
-    return $this->pdo->query($sql)->fetchColumn();
-}
-
-function sum($col,...$arg){
-    $sql="select sum($col) from $this->table ";
-    if(isset($arg[0])){
-        if(is_array($arg[0])){
-            $tmp=$this->arraytosql($arg[0]);
-            $sql=$sql." where ".join(" AND " , $tmp);
-
-        }else{
-            $sql .= $arg[0];
-        }
-    }
-
-    if(isset($arg[1])){
-        $sql .= $arg[1];
-    }
-
-    return $this->pdo->query($sql)->fetchColumn();
-}
-function max($col,...$arg){
-    $sql="select max($col) from $this->table ";
     if(isset($arg[0])){
         if(is_array($arg[0])){
             $tmp=$this->arraytosql($arg[0]);
--- back/admin.php
+++ back/admin.php
@@ -15,8 +15,8 @@
         <td><?=str_repeat("*",mb_strlen($admin['pw']));?></td>
         <td>
            <?php if($admin['acc']!='admin'):?>
-            <button data-id="<?=$admin['id'];?>" onclick="location.href='?do=edit_admin&id=<?=$admin['id'];?>'">修改</button>
-            <button data-id="<?=$admin['id'];?>">刪除</button>
+            <button onclick="location.href='?do=edit_admin&id=<?=$admin['id'];?>'">修改</button>
+            <button class='del-btn' data-id="<?=$admin['id'];?>">刪除</button>
             <?php else: ?>
                 此帳號為最高權限
             <?php endif; ?>
@@ -25,4 +25,19 @@
     <?php endforeach; ?>
  </table>
 
- <div class="ct"><button onclick="location.href='index.php'">返回</button></div>
+ <div class="ct">
+    <button onclick="location.href='index.php'">返回</button>
+</div>
+
+
+<script>
+$(".del-btn").on("click",function(){
+    let id=$(this).data("id");
+    if(confirm(`確定要刪除這筆管理員資料嗎?`)){
+        $.post("./api/del.php",{id,table:'Admin'},()=>{
+            location.reload();
+        })
+    }
+})
+
+</script>
