c59471f2e9e10e838bfb5b67ef6795bc48333a4d
Yuz <wwowkdrr@gmail.com>
2025-08-25 09:19:32 +0800

    admin修改

---
+++ admin.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-08-25 03:13:34
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db11`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text NOT NULL,
+  `pw` text NOT NULL,
+  `pr` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`, `pr`) VALUES
+(1, 'admin', '1234', 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}'),
+(2, 'test2', '12345', 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";}'),
+(3, 'test3', '1234', 'a:1:{i:0;s:1:\"1\";}');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- back.php
+++ back.php
@@ -1,66 +1,62 @@
 <?php include_once "./api/db.php";
 
-if (!isset($_SESSION['admin'])) {
+if(!isset($_SESSION['admin'])){
     to("index.php");
     exit();
 }
 
 
-$super = $Admin->find(['acc' => $_SESSION['admin']]);
-$super_pr = unserialize($super['pr']);
+$super=$Admin->find(['acc'=>$_SESSION['admin']]);
+$super_pr=unserialize($super['pr']);
 
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-    <title>┌精品電子商務網站」</title>
-    <link href="./css/css.css" rel="stylesheet" type="text/css">
-    <script src="./js/jquery-3.4.1.js"></script>
-    <script src="./js/js.js"></script>
+<title>┌精品電子商務網站」</title>
+<link href="./css/css.css" rel="stylesheet" type="text/css">
+<script src="./js/jquery-3.4.1.js"></script>
+<script src="./js/js.js"></script>
 </head>
 
 <body>
 
-    <div id="main">
-        <div id="top">
-            <a href="index.php">
-                <img src="./icon/0416.jpg">
+	<div id="main">
+    	<div id="top">
+        	<a href="index.php">
+            	<img src="./icon/0416.jpg">
             </a>
-            <img src="./icon/0417.jpg">
-        </div>
+                            <img src="./icon/0417.jpg">
+                   </div>
         <div id="left" class="ct">
-            <div style="min-height:400px;">
-                <?php
-                echo ($super['acc'] == 'admin') ? "<a href='?do=admin'>管理權限設置</a>" : '';
-                echo (in_array(1, $super_pr)) ? "<a href='?do=th'>商品分類與管理</a>" : '';
-                echo (in_array(2, $super_pr)) ? "<a href='?do=order'>訂單管理</a>" : '';
-                echo (in_array(3, $super_pr)) ? "<a href='?do=mem'>會員管理</a>" : '';
-                echo (in_array(4, $super_pr)) ? "<a href='?do=bot'>頁尾版權管理</a>" : '';
-                echo (in_array(5, $super_pr)) ? "<a href='?do=news'>最新消息管理</a>" : '';
+        	<div style="min-height:400px;">
+                <?php 
+                echo ($super['acc']=='admin')?"<a href='?do=admin'>管理權限設置</a>":'';
+            	echo (in_array(1,$super_pr))?"<a href='?do=th'>商品分類與管理</a>":'';
+            	echo (in_array(2,$super_pr))?"<a href='?do=order'>訂單管理</a>":'';
+            	echo (in_array(3,$super_pr))?"<a href='?do=mem'>會員管理</a>":'';
+            	echo (in_array(4,$super_pr))?"<a href='?do=bot'>頁尾版權管理</a>":'';
+            	echo (in_array(5,$super_pr))?"<a href='?do=news'>最新消息管理</a>":'';
                 ?>
-                <a href="./api/logout.php" style="color:#f00;">登出</a>
+            	<a href="./api/logout.php" style="color:#f00;">登出</a>
             </div>
         </div>
         <div id="right">
-            <?php
-            $do = $_GET['do'] ?? 'admin';
-            $file = "./back/{$do}.php";
-            if (file_exists($file)) {
+		<?php 
+        $do=$_GET['do']??'admin';
+        $file="./back/{$do}.php";
+        if(file_exists($file)){
                 include $file;
-            } else {
+
+        }else{
                 include "./back/admin.php";
-            }           
-            ?>                  
-        </div>
+        }
+        ?>	
+        	        </div>
         <div id="bottom" style="line-height:70px; color:#FFF; background:url(./icon/bot.png);" class="ct">
-            頁尾版權 : <?= $Bot->find(1)['bottom']; ?> </div>
+        	<?=$Bot->find(1)['bottom'];?>       </div>
     </div>
 
-</body>
-
-</html>
+</body></html>
