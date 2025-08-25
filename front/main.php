3aec4aa2671aae765837682d93a7cdcdbfcd6a8c
Yuz <wwowkdrr@gmail.com>
2025-08-25 11:34:15 +0800

    完成商品陳列及商品詳細資料

--- front/main.php
+++ front/main.php
@@ -1,8 +1,7 @@
-<?php
+<?php 
 $type=$_GET['type']??0;
-$row=$Type->find($type);
-
 $nav='全部商品';
+
 if($type!=0){
     $row=$Type->find($type);
     if($row['big_id']==0){
@@ -13,6 +12,7 @@ if($type!=0){
         $nav=$big['name']." > ".$row['name'];
         $items=$Item->all(['mid'=>$type,'sh'=>1]);
     }
+
 }else{
     $items=$Item->all(['sh'=>1]);
 }
@@ -21,10 +21,39 @@ if($type!=0){
 
 <h2><?=$nav;?></h2>
 
-<?php
-foreach($items as $item){
-    echo $item['name'];
-    echo "<br>";
-}
+<?php  foreach($items as $item): ?>
+<div style="display:flex;width:65%;margin:5px auto;height:150px;">
+    <div class="pp ct" style="padding:20px">
+        <a href="?do=detail&id=<?=$item['id'];?>">
+        <img src="./image/<?=$item['img'];?>" style="width:150px;height:100px;">
+        </a>
+    </div>
+    <div>
+        <table style="height:100%;">
+            <tr>
+                <td class="tt ct"><?=$item['name'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>價錢:<?=$item['price'];?>
+                <a href="?do=buycart&id=<?=$item['id'];?>&qt=1">
+                <img src="./icon/0402.jpg" style="float:right">
+                </a>
+            </td>
+            </tr>
+            <tr class="pp">
+                <td>規格:<?=$item['spec'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    簡介:<?=mb_substr($item['intro'],0,20);?>...
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
 
-?>
+
+
+
+
+<?php endforeach; ?>
