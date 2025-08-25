4eba40a29182d6b1ed81e0d5b16dcabc740ff0c2
Yuz <wwowkdrr@gmail.com>
2025-08-25 14:17:45 +0800

    購物車的session

--- front/buycart.php
+++ front/buycart.php
@@ -1,7 +1,24 @@
 <?php
+
+if(isset($_GET['id'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+
 if(!isset($_SESSION['login'])){
     to("?do=login");
 }
 ?>
 
+
+
 <h2 class="ct"><?=$_SESSION['login'];?>的購物車</h2>
+
+<?php
+if(isset($_SESSION['cart']) && count($_SESSION['cart'])>0){
+    dd($_SESSION['cart']);
+
+}else{
+    echo "購物車是空的";
+}
+
+?>
