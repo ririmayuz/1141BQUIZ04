c03e2e336dc1b56954b98440123be6c234810a15
Yuz <wwowkdrr@gmail.com>
2025-08-25 10:53:09 +0800

    根據type來取得商品資料

--- front/main.php
+++ front/main.php
@@ -1,1 +1,30 @@
-商品
+<?php
+$type=$_GET['type']??0;
+$row=$Type->find($type);
+
+$nav='全部商品';
+if($type!=0){
+    $row=$Type->find($type);
+    if($row['big_id']==0){
+        $nav=$row['name'];
+        $items=$Item->all(['big'=>$type,'sh'=>1]);
+    }else{
+        $big=$Type->find($row['big_id']);
+        $nav=$big['name']." > ".$row['name'];
+        $items=$Item->all(['mid'=>$type,'sh'=>1]);
+    }
+}else{
+    $items=$Item->all(['sh'=>1]);
+}
+
+?>
+
+<h2><?=$nav;?></h2>
+
+<?php
+foreach($items as $item){
+    echo $item['name'];
+    echo "<br>";
+}
+
+?>
